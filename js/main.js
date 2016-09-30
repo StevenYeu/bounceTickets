a5352c67fefabaa9b94b1d85986794ee0911a0f7
Steven Yeu <yeusteven@gmail.com>
2016-09-30 08:38:45 -0700

    add functionality ti update data

--- js/main.js
+++ js/main.js
@@ -21,121 +21,124 @@ $(document).ready(function () {
  });
 
  function update() {
-	 var json = $.getJSON( "http://holonet.sdsc.edu:8134/requestData", function(data) {
-			 console.log( "success" );
-			 console.log(data);
-			 })
+	 var json = $.getJSON( "http://holonet.sdsc.edu:8134/requestData", function(data){
+		 processValidData(data);
+		 processWeirdData(data);
+	 });
+ }
+
+ function processValidData(data) {
+	 valid = data.validProjects;
+	 $.each(valid, function(index) { //llop through each project
+		 var items = [];
+		 items.push("<tr>");
+		 // Create Email Button
+		 items.push("<td align='center'><a class='btn btn-default'><em class='fa fa-paper-plane-o'></em></a></td>")
+
+		 // Add Dates
+		 items.push("<td>")
+		 for(i=0; i < valid[index].emailDates.length; i++) {
+			 var unix = valid[index].emailDates[i];
+			 var timestamp = moment.unix(unix);
+			 var dates = timestamp.format("MM-DD-YYYY");
+			 console.log(dates);
+			 items.push(dates);
+		 }
+		 items.push("</td>");
+
+		 //Add number of flags
+		 items.push("<td>");
+		 items.push(valid[index].ticketNumbers.length.toString());
+		 items.push("</td>");
+
+		 // Add the Status of Project
+		 items.push("<td>");
+		 items.push(valid[index].warningLevel);
+		 items.push("</td>")
+
+		 // Add Ticket Numbers
+		 items.push("<td>");
+		 for(i=0; i < valid[index].ticketNumbers.length; i++) {
+			 var nums = valid[index].ticketNumbers[i].toString()+"\n";
+			 items.push(nums);
+		 }
+		 items.push("</td>");
+
+		 // Add Emails
+		 items.push("<td>");
+		 for(i=0; i < valid[index].contactEmail.length; i++) {
+			 var emails = valid[index].contactEmail[i].toString()+"\n";
+			 items.push(emails);
+		 }
+		 items.push("</td>");
+
+		 //Add Name
+		 items.push("<td>");
+		 items.push(valid[index].projectName);
+		 items.push("</td>");
+
+		 //Add Types
+		 items.push("<td>");
+		 for(i=0; i < valid[index].failingType.length; i++) {
+			 var types = valid[index].failingType[i].toString()+"\n";
+			 items.push(types);
+		 }
+		 items.push("</td>");
+
+		 items.push("</tr>");
+		 var row = items.join("");
+		 $('#valid').append(row);
+	 });
+ }
+
+
+ function processWeirdData(data) {
+	 weird = data.weirdTickets;
+	 $.each(weird, function(index) { //llop through each project
+		 var items = [];
+		 items.push("<tr>");
+		 // Create Email Button
+		 items.push("<td align='center'><a class='btn btn-default'><em class='fa fa-paper-plane-o'></em></a></td>")
+
+		 // Add ticket Numbers
+		 items.push("<td>");
+		 items.push(weird[index].ticketNumber);
+		 items.push("</td>");
+
+		 //Add Name
+		 items.push("<td>");
+		 items.push(weird[index].projectName);
+		 items.push("</td>");
+
+		 //Add Month
+		 items.push("<td>");
+		 items.push(weird[index].month.toString());
+		 items.push("</td>");
+
+		 //Add Types
+		 items.push("<td>");
+		 items.push(weird[index].failingType);
+		 items.push("</td>");
+
+		 items.push("</tr>");
+		 var row = items.join("");
+		 $('#weird').append(row);
+
+	 });
  }
 
 /****
 	Loads Valid Project Data into Tables
 ****/
  function loadValidData() {
-
 	 $.getJSON( "data.json", function(data) {
-			valid = data.validProjects;
-			$.each(valid, function(index) { //llop through each project
-				var items = [];
-				items.push("<tr>");
-				// Create Email Button
-				items.push("<td align='center'><a class='btn btn-default'><em class='fa fa-paper-plane-o'></em></a></td>")
-
-				// Add Dates
-				items.push("<td>")
-				for(i=0; i < valid[index].emailDates.length; i++) {
-					var unix = valid[index].emailDates[i];
-					var timestamp = moment.unix(unix);
-					var dates = timestamp.format("MM-DD-YYYY");
-					console.log(dates);
-					items.push(dates);
-				}
-				items.push("</td>");
-
-				//Add number of flags
-				items.push("<td>");
-				items.push(valid[index].ticketNumbers.length.toString());
-				items.push("</td>");
-
-				// Add the Status of Project
-				items.push("<td>");
-				items.push(valid[index].warningLevel);
-				items.push("</td>")
-
-				// Add Ticket Numbers
-				items.push("<td>");
-				for(i=0; i < valid[index].ticketNumbers.length; i++) {
-					var nums = valid[index].ticketNumbers[i].toString()+"\n";
-					items.push(nums);
-				}
-				items.push("</td>");
-
-				// Add Emails
-				items.push("<td>");
-				for(i=0; i < valid[index].contactEmail.length; i++) {
-					var emails = valid[index].contactEmail[i].toString()+"\n";
-					items.push(emails);
-				}
-				items.push("</td>");
-
-				//Add Name
-				items.push("<td>");
-				items.push(valid[index].projectName);
-				items.push("</td>");
-
-				//Add Types
-				items.push("<td>");
-				for(i=0; i < valid[index].failingType.length; i++) {
-					var types = valid[index].failingType[i].toString()+"\n";
-					items.push(types);
-				}
-				items.push("</td>");
-
-				items.push("</tr>");
-				var row = items.join("");
-				$('#valid').append(row);
-			});
-	 })
+		 processValidData(data);
+	 });
  }
 
 
  function loadWeirdData() {
-
 	 $.getJSON( "data.json", function(data) {
-		 weird = data.weirdTickets;
-		 console.log(weird);
-		 $.each(weird, function(index) { //llop through each project
-			 var items = [];
-			 items.push("<tr>");
-			 // Create Email Button
-			 items.push("<td align='center'><a class='btn btn-default'><em class='fa fa-paper-plane-o'></em></a></td>")
-
-			 // Add ticket Numbers
-			 items.push("<td>");
-			 items.push(weird[index].ticketNumber);
-			 items.push("</td>");
-
-			 //Add Name
-			 items.push("<td>");
-			 items.push(weird[index].projectName);
-			 items.push("</td>");
-
-			 //Add Month
-			 items.push("<td>");
-			 items.push(weird[index].month.toString());
-			 items.push("</td>");
-
-			 //Add Types
-			 items.push("<td>");
-			 items.push(weird[index].failingType);
-			 items.push("</td>");
-
-			 items.push("</tr>");
-			 var row = items.join("");
-			 $('#weird').append(row);
-
-		 });
-
+		 processWeirdData(data);
 	 });
-
-
  }
